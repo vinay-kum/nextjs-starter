f447964abcc053da9441a60471a30b634c401d4e
vinay-kumar <mailbox4vinay@gmail.com>
2024-03-17 23:06:32 +0530

    Added shadcn support

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline text-blue-500">
-      Added Tailwind CSS
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline text-blue-500">
+        Added Tailwind CSS
+      </h1>
+      <Button>Shadcn Button</Button>
+    </>
 
   );
 }
